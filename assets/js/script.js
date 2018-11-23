c2221ff1e188bb4b2bff2d0f1324f5321875cb61
Victor Joseph <9651630+victorvasujoseph@users.noreply.github.com>
2018-11-22 19:03:43 -0800

    Technology Icons

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+
+    // $("#my-bio-link").click(function() {
+    //     console.log("Hello");
+    //     $('html,body').animate({
+    //         scrollTop: $("#bio").offset().top},
+    //         'slow');
+    // });
+
+});
